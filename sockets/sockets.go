2a2292cf463ce0968f839ff744ca1412fcece623
BeauRussell <yourknightmares@gmail.com>
2024-12-22 11:21:57 -0600

    Simplify creating TCP/UDP connections for now

---
+++ sockets/sockets.go
@@ -0,0 +1,52 @@
+package sockets
+
+import (
+	"fmt"
+	"net"
+	"strconv"
+)
+
+type socketConnection interface {
+	CreateConnection()
+}
+
+type TCPConnection struct {
+	host     string
+	port     int
+	listener net.Listener
+}
+
+type UDPConnection struct {
+	host     string
+	port     int
+	listener *net.UDPConn
+}
+
+func (tcp *TCPConnection) CreateConnection() net.Listener {
+	conn, err := net.Listen("tcp", tcp.host+":"+strconv.Itoa(int(tcp.port)))
+	if err != nil {
+		fmt.Println("Error setting up TCP listener:", err)
+		panic(err)
+	}
+	defer conn.Close()
+
+	tcp.listener = conn
+
+	return conn
+}
+
+func (udp *UDPConnection) CreateConnection() *net.UDPConn {
+	addr := net.UDPAddr{
+		Port: udp.port,
+		IP:   net.ParseIP(udp.host),
+	}
+
+	conn, err := net.ListenUDP("udp", &addr)
+	if err != nil {
+		fmt.Println("Error setting up UDP listener:", err)
+		panic(err)
+	}
+
+	udp.listener = conn
+	return conn
+}
