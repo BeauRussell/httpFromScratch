2eefa59db5eddd2a0464e46c8e5e797fb2af5dbf
BeauRussell <yourknightmares@gmail.com>
2024-12-24 09:47:59 -0600

    Set up the most basic http server from TCP connection

--- http1/http1.go
+++ http1/http1.go
@@ -1,6 +1,12 @@
 package http1
 
-import "httpFromScratch/sockets"
+import (
+	"bufio"
+	"fmt"
+	"httpFromScratch/sockets"
+	"net"
+	"strings"
+)
 
 func Server() {
 	connection := sockets.TCPConnection{
@@ -9,6 +15,7 @@ func Server() {
 	}
 
 	listener := connection.CreateConnection()
+	defer listener.Close()
 
 	for {
 		conn, err := listener.Accept()
@@ -22,4 +29,61 @@ func Server() {
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
+
+	reader := bufio.NewReader(conn)
+	for {
+		requestLine, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading request line:", err)
+			return
+		}
+		requestLine = strings.TrimSpace(requestLine)
+		fmt.Println("Request Line:", requestLine)
+
+		headers := make(map[string]string)
+		for {
+			line, err := reader.ReadString('\n')
+			if err != nil {
+				fmt.Println("Error reading headers:", err)
+			}
+			line = strings.TrimSpace(line)
+			if line == "" {
+				break
+			}
+			parts := strings.SplitN(line, ":", 2)
+			if len(parts) == 2 {
+				headers[strings.TrimSpace(parts[0])] = strings.TrimSpace(parts[1])
+			}
+		}
+
+		handleHTTPRequest(conn, requestLine, headers)
+	}
+}
+
+func handleHTTPRequest(conn net.Conn, requestLine string, headers map[string]string) {
+	parts := strings.Split(requestLine, " ")
+	if len(parts) < 3 {
+		writeHTTPResponse(conn, 400, "Bad Request", "Invalid request line")
+		return
+	}
+
+	method, path, version := parts[0], parts[1], parts[2]
+	fmt.Printf("Method: %s, Path: %s, Version %s\n", method, path, version)
+
+	if path == "/" {
+		writeHTTPResponse(conn, 200, "OK", "<h1>Welcome to the GO HTTP server!</h1>")
+	} else {
+		writeHTTPResponse(conn, 404, "Not Found", "<h1>Page not found</h1>")
+	}
+}
+
+func writeHTTPResponse(conn net.Conn, statusCode int, statusText string, bodyText string) {
+	response := fmt.Sprintf(
+		"HTTP/1.1 %d %s\r\n"+
+			"Content-Length: %d\r\n"+
+			"Content-Type: text/html\r\n"+
+			"Connection: close\r\n"+
+			"\r\n%s",
+		statusCode, statusText, len(bodyText), bodyText)
+	conn.Write([]byte(response))
 }
